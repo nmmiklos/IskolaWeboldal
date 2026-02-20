75605cf4389b5df1f963e147ff7380bdc57d3222
NotTheMark <145426266+NotTheMark@users.noreply.github.com>
2026-02-20 01:32:58 +0100

    Dinamikusan változó számlálók  a home indexbe Átrendezése a nav barnak és a menü kibővítése Órarend és egyéb végpontok létrehozása CSS igazítás, bug fixek ( eltűnt a sárga csík, a nem működő box-shadow) Hero részleg újra rendezése, iskola nevének kiírása Iskola kép hozzáadása a right heroba, az alá tervezett gombok elrendezése Favicon lecserélése az iskola logóra

---
+++ VBJWeboldal/Controllers/InformationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VBJWeboldal.Controllers
+{
+    public class InformationsController : Controller
+    {
+        public IActionResult Timetable()
+        {
+            return View();
+        }
+    }
+}
