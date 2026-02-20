68c802123d839e5f547ad0b4a3810ce3402f42de
Mudris <mudris.balazs@gmail.com>
2026-02-20 10:21:19 +0100

    Hírek

--- VBJWeboldal/Controllers/HirekController.cs
+++ VBJWeboldal/Controllers/HirekController.cs
@@ -1,14 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VBJWeboldal.Data;
+using VBJWeboldal.ViewModels;
+using VBJWeboldal.Models;
 
 namespace VBJWeboldal.Controllers
 {
-    [Route("hirek")]
+    // Megadjuk az alap útvonalat
+    [Route("Hirek")]
     public class HirekController : Controller
     {
-        [HttpGet("")]
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public HirekController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        // Elérhető: /Hirek vagy /Hirek/Index
+        [HttpGet]
+        [HttpGet("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var hirekListaja = await _context.News // Itt a te táblanevedet (News) használtam
+                .OrderByDescending(h => h.PublishedAt)
+                .ToListAsync();
+
+            var viewModel = new NewsListViewModel
+            {
+                News = hirekListaja
+            };
+
+            return View(viewModel);
+        }
+
+        // Elérhető: /Hirek/Details/5
+        // Fontos: Itt kötelezővé tesszük az ID-t a route-ban, így nem keveredik az Index-szel
+        [HttpGet("Details/{id}")]
+        public async Task<IActionResult> Details(int id)
+        {
+            // Javított lekérdezés a piros hullámvonal ellen:
+            var cikk = await _context.News.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (cikk == null)
+            {
+                return NotFound();
+            }
+            var viewModel = new NewsDetailsViewModel
+            {
+                HirItem = cikk
+            };
+            return View(viewModel);
         }
     }
 }
